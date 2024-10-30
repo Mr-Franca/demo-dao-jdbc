e802a26ac727c0df725ef88a0ebba40cf04181ac
Alex Silva <alex._.silva@outlook.com>
2024-10-30 14:05:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,9 +6,9 @@ import model.entities.Seller;
 
 public interface SellerDao {
 
-	void insert(Department obj);
-	void update(Department obj);
+	void insert(Seller obj);
+	void update(Seller obj);
 	void deleteByID(Integer id);
 	Seller findById(Integer id);
-	List<Department> findAll();
+	List<Seller> findAll();
 }
