c9b2b561357ef689038472aebda7c5dcd213d532
Alex Silva <alex._.silva@outlook.com>
2024-10-31 17:00:54 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,13 +12,12 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerdao = DaoFactory.createSellerDao(); 
-		Department d = new Department();
-		d.setId(2);
-		System.out.println("TESTE INSERT");
+		System.out.println("Teste update");
 		
-		Seller newSeller = new Seller(null, "Greg","Greg@gmail.com",new Date(), 4000.0,d);
-		sellerdao.insert(newSeller);
-		System.out.println("Inserted! New id = "+newSeller.getId());
+		Seller seller = sellerdao.findById(1);
+		seller.setName("Martha Waine");
+		sellerdao.update(seller);
+		System.out.println("Update complete");
 		}
 
 }
